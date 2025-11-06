3625a802d5c6817aef17253cb19c8d0d7d43b84a
Tony Yunker <tony.yunker@gmail.com>
2025-11-06 16:34:21 -0600

    refactor: separate routes for each optimization

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,16 @@
 class AuthorsController < ApplicationController
-  def index
+  def unoptimized
     @authors = Author.all.order(:name)
+    render :index
+  end
+
+  def partially_optimized
+    @authors = Author.includes(:posts).all.order(:name)
+    render :index
+  end
+
+  def optimized
+    @authors = Author.includes(posts: [:tags]).all.order(:name)
+    render :index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :authors, only: [:index]
+  # resources :authors, only: [:index]
+
+  scope "/authors" do
+    get "/unoptimized", to: "authors#unoptimized", as: :unoptimized
+    get "/partially_optimized", to: "authors#partially_optimized", as: :partially_optimized
+    get "/optimized", to: "authors#optimized", as: :optimized
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.

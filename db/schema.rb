5024805b51a28ba079c62226aafabd6b631e75fa
Tony Yunker <tony.yunker@gmail.com>
2025-11-06 16:33:55 -0600

    feat: add tags

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_10_28_204051) do
+ActiveRecord::Schema[8.1].define(version: 2025_10_28_213703) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -29,5 +29,14 @@ ActiveRecord::Schema[8.1].define(version: 2025_10_28_204051) do
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
+  create_table "tags", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name", null: false
+    t.bigint "post_id"
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_tags_on_post_id"
+  end
+
   add_foreign_key "posts", "authors"
+  add_foreign_key "tags", "posts"
 end
--- lib/tasks/generate_data.rake
+++ lib/tasks/generate_data.rake
@@ -1,17 +1,24 @@
 task generate_data: :environment do
-  (1..20).each do |i|
+  (1..100).each do |i|
     author = Author.create(name: "Author #{i}")
 
-    post_count = [3, 4, 5].sample
+    post_count = [10].sample
     (1..post_count).each do |j|
-      Post.create(
+      p = Post.create(
         title: "Author #{i}'s #{j.ordinalize} Post",
         author:,
         body: "some body"
       )
+
+      Tag.create(name: "ruby", post: p) # if rand > 0.5
+      Tag.create(name: "rails", post: p) # if rand > 0.5
+      Tag.create(name: "postgres", post: p) # if rand > 0.5
+      Tag.create(name: "elixir", post: p) # if rand > 0.5
+      Tag.create(name: "react", post: p) # if rand > 0.5
     end
   end
 
   puts "Created #{Author.count} Authors"
   puts "Created #{Post.count} Posts"
+  puts "Created #{Tag.count} Tags"
 end
